150490418bcdb03a6f2febf465b1ffe91874aafd
yst <463861615@qq.com>
2018-11-09 11:05:55 +0800

    测试

---
+++ sso-client/src/main/java/cn/jenkins/CsController.java
@@ -0,0 +1,13 @@
+package cn.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsController {
+
+	@GetMapping("cs")
+	public String cs(){
+		return "测试成功了！";
+	}
+}
---
+++ sso-client/src/main/java/cn/jenkins/JenKinsApplication.java
@@ -0,0 +1,20 @@
+package cn.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+@SpringBootApplication
+public class JenKinsApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenKinsApplication.class, args);
+	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenKinsApplication.class);
+    }
+}
